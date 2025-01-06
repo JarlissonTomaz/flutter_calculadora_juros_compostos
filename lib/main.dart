ec7d591168c81e6a47fe765ff8cd08fa2fdfba94
JarlissonTomaz <tomazj995@gmail.com>
2025-01-06 14:17:20 -0300

    build: home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 import 'package:flutter_calculadora_impostos/screen/home_app.dart';
 
--- lib/screen/home_app.dart
+++ lib/screen/home_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculadora_impostos/screen/compound_interest_calculator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Home App'),)
+      ,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder:  (context) => CalculadoraJC() ))  ;
+            },
+            child: Text('Calculadora Juros Compostos'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
